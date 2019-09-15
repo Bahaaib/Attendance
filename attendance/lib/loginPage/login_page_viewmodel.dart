527ce2d38986e3ff950a49c9734333ed8a3e1080
Bahaaib <bahaasco@gmail.com>
2019-09-15 21:58:13 +0200

    Fixed dependancy conflicts with AndroidX

--- attendance/lib/loginPage/login_page_viewmodel.dart
+++ attendance/lib/loginPage/login_page_viewmodel.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
 /// view-model of login page
 abstract class LoginPageViewModel extends State<LoginPage> {
   /// constructor
@@ -15,18 +16,21 @@ abstract class LoginPageViewModel extends State<LoginPage> {
       }
     });
   }
+
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn =  GoogleSignIn();
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   /// call signIn routine
   Future<FirebaseUser> signInUser() async {
     final GoogleSignInAccount googleSignInAccount =
-      await _googleSignIn.signIn();
+        await _googleSignIn.signIn();
     final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
-    final FirebaseUser firebaseUser = await _auth.signInWithGoogle(
+        await googleSignInAccount.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
         idToken: googleSignInAuthentication.idToken,
         accessToken: googleSignInAuthentication.accessToken);
+    final FirebaseUser firebaseUser =
+        await _auth.signInWithCredential(credential);
     return firebaseUser;
   }
 
@@ -40,11 +44,11 @@ abstract class LoginPageViewModel extends State<LoginPage> {
           timeInSecForIos: 1);
     } else {
       User()
-      ..nameMe(user.displayName)
-      ..assignEmail(user.email)
-      ..changePicture(user.photoUrl)
-      ..loadInfo()
-      ..save();
+        ..nameMe(user.displayName)
+        ..assignEmail(user.email)
+        ..changePicture(user.photoUrl)
+        ..loadInfo()
+        ..save();
       Navigator.of(context).pushReplacementNamed('/home');
     }
   }
